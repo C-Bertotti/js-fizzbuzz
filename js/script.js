d390c90c23026a45dd7fee836957472210fc7729
Caterina Bertotti <bertotti4997@gmail.com>
2021-03-02 18:00:07 +0100

    FizzBuzz!!

--- js/script.js
+++ js/script.js
@@ -2,13 +2,14 @@
 ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
 
-//Stampo i numeri da 1 a 100
-var number = [];
-
 for (var i = 1; i <= 100; i++) {
-    number.push(i);
- 
-    
-    document.getElementById('fizzbuzz__list').innerHTML += "<li>" + number + "</li>";
-   
+    if (i % 5 == 0 && i % 3 == 0) {
+        document.getElementById('fizzbuzz__list').innerHTML += "<li>FizzBuzz</li>";  
+    } else if (i % 5 == 0 ) {
+        document.getElementById('fizzbuzz__list').innerHTML += "<li>Buzz</li>";  
+    } else if (i % 3 == 0) {
+        document.getElementById('fizzbuzz__list').innerHTML += "<li>Fizz</li>";  
+    } else {
+        document.getElementById('fizzbuzz__list').innerHTML += "<li>" + i + "</li>";
+    }  
 }
